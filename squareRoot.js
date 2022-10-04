536e8ae691b70eb1f1bf07f91ccbe249cfd8cecf
dansinfosec <danberger08@gmail.com>
2022-10-04 15:28:21 +0100

    second-commit

--- squareRoot.js
+++ squareRoot.js
@@ -1,5 +1,32 @@
 // Eloquent Javascript
-let theNumber = Number(prompt("Pick a number"));
+let theNumber = 15 //theNumber(prompt("Pick a number"));
 if (!Number.isNaN(theNumber)) {
-	console.log("Your number is the square root of " + theNumber * theNumber)
+	console.log("The square root of your number is (15) " + theNumber * theNumber)
 }
+
+// Long unreadable approach to subsequently add numbers in a certain range (1-11)
+let total = 0, count = 1;
+while (count <= 10) {
+	total += count;
+	count +=1
+}
+console.log(total);
+// -> 55
+
+// var sum=0;
+// // Short readable approach to subsequently add numbers in a certain range(1-11)
+// console.log(sum(range(1,10)));
+// // -> Error output??? ReferenceError: sum is not defined
+
+function factorial(n){
+	if (n == 0) {
+		return 1;
+	} else {
+		return factorial(n-1) * n;
+	}
+}
+
+console.log(factorial(8));
+// -> 40320
+
+console.log(1-1*3)
